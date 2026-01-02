54434cebba1d4606a9fb9872daba13b492b27bf4
Ntchinda-Giscard <ntchinda1998@gmail.com>
2026-01-02 14:56:47 +0100

    Add CORS middleware and health check endpoint to FastAPI application

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from fastapi import FastAPI
-
+from src.routes.event_route import events_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+# Incude middleware, CORS, etc. if needed
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+
+
+app.include_router(events_router)
+
+@app.get("/health")
+def read_root():
+    return {"API": "Healthy"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
